3fe578dff51c6801310f29fb2dae87a261a1572b
syntin <rgc.syntin@gmail.com>
2026-08-26 18:56:16 -0400

    Fixed code to compile correctly. no idea if I did it right ... welcome to Vulkan.

--- Source/vulkanQueue.cpp
+++ Source/vulkanQueue.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include "vulkanQueue.h"
 
+#define MAX_DEVICE_COUNT 64
 
 VulkanQueue::VulkanQueue()
 {
@@ -18,23 +19,28 @@ VulkanQueue::~VulkanQueue()
 void VulkanQueue::Initialize(VkInstance instance)
 {
     VkPhysicalDeviceProperties2 deviceProperties{ .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PROPERTIES_2 };
-    VkPhysicalDevice* physicalDevice{};
+    VkPhysicalDevice physicalDevice[MAX_DEVICE_COUNT] = {};
     uint32_t deviceIndex = 0;
     vkGetPhysicalDeviceProperties2(physicalDevice[deviceIndex], &deviceProperties);
     std::cout << "Selected device: " << deviceProperties.properties.deviceName << "\n";
 
-    uint32_t queueFamilyCount{ 0 };
+    uint32_t queueFamilyCount{};
     vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice[deviceIndex], &queueFamilyCount, nullptr);
     std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
     vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice[deviceIndex], &queueFamilyCount, queueFamilies.data());
-    uint32_t queueFamily{ 0 };
-    for (size_t i = 0; i < queueFamilies.size(); i++) {
-        if (queueFamilies[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
-            queueFamily = i;
+    uint32_t queueFamily{};
+    for (size_t i = 0; i < queueFamilies.size(); i++)
+    {
+        if (queueFamilies[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)
+        {
+            queueFamily = static_cast<uint32_t>(i);
             break;
         }
     }
-    if(SDL_Vulkan_GetPresentationSupport(instance, physicalDevice[deviceIndex], queueFamily) == VK_TRUE);
+    if (SDL_Vulkan_GetPresentationSupport(instance, physicalDevice[deviceIndex], queueFamily) == VK_FALSE)
+    {
+        throw std::runtime_error("Failed to create presentation layer");
+    }
     const float qfpriorities{ 1.0f };
     VkDeviceQueueCreateInfo queueCI{
         .sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
@@ -69,8 +75,7 @@ void VulkanQueue::Initialize(VkInstance instance)
     .ppEnabledExtensionNames = deviceExtensions.data(),
     .pEnabledFeatures = &enabledVk10Features
     };
-    VkDevice device;
-    VkQueue queue;
-    if(vkCreateDevice(physicalDevice[deviceIndex], &deviceCI, nullptr, &device) == VK_TRUE);
+    VkDevice device{};
+    VkQueue queue{};
     vkGetDeviceQueue(device, queueFamily, 0, &queue);
 }
