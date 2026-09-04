5f59cbd4eaf28b363f2b330a08fee72200a865a4
Roland Cozzolino <rgc.syntin@gmail.com>
2026-09-04 10:18:26 -0400

    resolved engine

--- Source/engine.h
+++ Source/engine.h
@@ -12,12 +12,6 @@ public:
 	Engine();
 	virtual ~Engine();
 
-<<<<<<< HEAD
-public:
-	void Inititalize();
-/*
-=======
->>>>>>> 932cff45debb381f728ecea216068e4fea6977b9
 public:
 	void Initialize();
 };
