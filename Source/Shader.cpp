c7f0e7efda13433331291016a9bd4411e396f46e
dmbetlej <dmbetlej@gmail.com>
2026-09-09 14:34:57 -0400

    I really hope this works

--- Source/Shader.cpp
+++ Source/Shader.cpp
@@ -1,4 +1,4 @@
-// Author: RC
+// Author: RC & DB
 
 #include "shader.h"
 #include "utils.h"
@@ -12,6 +12,16 @@
 #include <iostream>
 #include <fstream>
 
+// Helper to read file into string
+static std::string ReadTextFile(const std::string& path)
+{
+	std::ifstream file(path);
+	if (!file.is_open())
+		return "";
+
+	return std::string((std::istreambuf_iterator<char>(file)),
+						std::istreambuf_iterator<char>());
+}
 
 Shader::Shader()
 {
@@ -25,9 +35,10 @@ Shader::~Shader()
 
 VkShaderModule Shader::CreateShaderModule(Device& device, const std::string& fileName, shaderc_shader_kind kind) const
 {
-	/*
+	// Load source
 	const std::string shaderPath = "src/shaders/" + fileName;
-	const std::string src = "";// readTextFile(shaderPath);
+	const std::string src = ReadTextFile(shaderPath);
+
 	if (src.empty())
 	{
 		showError("Specified shader file doesn't exist: " + shaderPath, nullptr);
@@ -38,43 +49,44 @@ VkShaderModule Shader::CreateShaderModule(Device& device, const std::string& fil
 	std::cout << "Compiling shader: " << shaderPath << std::endl;
 	shaderc::Compiler compiler;
 	shaderc::CompileOptions opts;
-	opts.SetTargetEnvironment(shaderc_target_env_vulkan, shaderc_env_version_vulkan_1_4);
-	opts.SetTargetSpirv(shaderc_spirv_version_1_6);
+
+	opts.SetTargetEnvironment(shaderc_target_env_vulkan, shaderc_env_version_vulkan_1_2);
 	opts.SetOptimizationLevel(shaderc_optimization_level_performance);
+
 	shaderc::CompilationResult result = compiler.CompileGlslToSpv(src, kind, fileName.c_str(), opts);
 
 	if (result.GetCompilationStatus() != shaderc_compilation_status_success)
 	{
 		std::cerr << "Shader Compilation Error: " << result.GetErrorMessage() << std::endl;
-		return nullptr;
+		return VK_NULL_HANDLE;
 	}
+
 	std::vector<uint32_t> spv = { result.cbegin(), result.cend() };
-	*/
+	
 	// pass spir-v to vulkan and create shader-module
-	VkShaderModuleCreateInfo moduleCreateInfo
-	{
-		.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
-		//.codeSize = spv.size() * sizeof(uint32_t),
-		//.pCode = spv.data()
-	};
-	VkShaderModule shaderModule = nullptr;
+	VkShaderModuleCreateInfo moduleCreateInfo{};
+	moduleCreateInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+	moduleCreateInfo.codeSize = spv.size() * sizeof(uint32_t);
+	moduleCreateInfo.pCode = spv.data();
+
+	VkShaderModule shaderModule = VK_NULL_HANDLE;
 	if (vkCreateShaderModule(device.GetLogicalDevice(), &moduleCreateInfo, nullptr, &shaderModule) != VK_SUCCESS)
 	{
 		showError("Error creating shader module", nullptr);
-		return nullptr;
+		return VK_NULL_HANDLE;
 	}
 	return shaderModule;
 }
 
 bool Shader::Create(Device& device)
 {
-	if (_vertShader = CreateShaderModule(device, "shader.vert", shaderc_vertex_shader); shaderc_vertex_shader)
-	{
+	_vertShader = CreateShaderModule(device, "shader.vert", shaderc_vertex_shader);
+	if (_vertShader == VK_NULL_HANDLE)
 		return false;
-	}
-	if (_fragShader = CreateShaderModule(device, "shader.frag", shaderc_fragment_shader); shaderc_fragment_shader)
-	{
+
+	_fragShader = CreateShaderModule(device, "shader.frag", shaderc_fragment_shader);
+	if (_fragShader == VK_NULL_HANDLE)
 		return false;
-	}
+
 	return true;
 }
