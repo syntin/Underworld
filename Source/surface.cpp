5dbab6a7d309935a16ecffe769cf16652d84af37
Roland Cozzolino <unclemilford@gmail.com>
2026-09-24 14:05:10 -0400

    Corrected typoschanged printf to lx from x

--- Source/surface.cpp
+++ Source/surface.cpp
@@ -15,7 +15,7 @@ bool Surface::Create(VkInstance& instance, SDL_Window* window)
 {
 	_surface = VK_NULL_HANDLE;
 
-	printf("Surface::Create — window=%p  flags=0x%x\n",
+	printf("Surface::Create — window=%p  flags=0lx%lx\n",
 		(void*)window, SDL_GetWindowFlags(window));
 
 	if (!SDL_Vulkan_CreateSurface(window, instance, nullptr, &_surface))
